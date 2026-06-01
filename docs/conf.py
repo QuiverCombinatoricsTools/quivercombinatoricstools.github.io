fb66677f8e772be5317021c826513d3cdc73fb8f
Emanuel Roth <eroth264@gmail.com>
2026-06-01 14:09:04 +0100

    Renamed website

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_title = "QuiverCombinatoricsTools"
 html_theme = "furo"
 html_static_path = ["_static"]
 html_css_files = [
